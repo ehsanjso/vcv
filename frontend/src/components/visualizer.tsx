25549a6291dc4bbf6d5faefb86eeffbd293c7c2e
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-19 10:56:35 -0400

    Add visualizer

---
+++ frontend/src/components/visualizer.tsx
@@ -0,0 +1,14 @@
+import { Badge } from '@/components/ui/badge';
+
+export default function Visualizer() {
+  return (
+    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-3">
+        <Badge variant="outline" className="absolute right-3 top-3">
+          Output
+        </Badge>
+        <div className="flex-1" />
+      </div>
+    </main>
+  );
+}
