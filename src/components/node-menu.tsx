ac327aecdfa8a95ed7d55de9c972c5cc2d2fff13
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-23 03:26:22 -0400

    Add node menu with template dialogs

---
+++ src/components/node-menu.tsx
@@ -0,0 +1,104 @@
+import { NodeToolbar, Position } from "@xyflow/react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { Button } from "./ui/button";
+import { CirclePlus, CircleX, GitBranch, Pencil } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function NodeMenu({ isLeaf }: { isLeaf: boolean }) {
+  return (
+    <Dialog>
+      <NodeToolbar isVisible={undefined} position={Position.Right}>
+        <nav className="grid gap-1 overflow-hidden rounded-xl border-2 border-border/50 bg-background">
+          {isLeaf ? (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <DialogTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="rounded-lg"
+                    aria-label="Add Node"
+                  >
+                    <CirclePlus className="size-5" />
+                  </Button>
+                </DialogTrigger>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Add Node
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <DialogTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="rounded-lg"
+                    aria-label="Branch Off"
+                  >
+                    <GitBranch className="size-5" />
+                  </Button>
+                </DialogTrigger>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Branch Off
+              </TooltipContent>
+            </Tooltip>
+          )}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DialogTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="rounded-lg"
+                  aria-label="Edit Node"
+                >
+                  <Pencil className="size-5" />
+                </Button>
+              </DialogTrigger>
+            </TooltipTrigger>
+            <TooltipContent side="right" sideOffset={5}>
+              Edit Node
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DialogTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="rounded-lg"
+                  aria-label="Delete Node"
+                >
+                  <CircleX className="size-5" />
+                </Button>
+              </DialogTrigger>
+            </TooltipTrigger>
+            <TooltipContent side="right" sideOffset={5}>
+              Delete Node
+            </TooltipContent>
+          </Tooltip>
+        </nav>
+      </NodeToolbar>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/node.tsx
+++ src/components/node.tsx
@@ -1,12 +1,5 @@
-import {
-  NodeToolbar,
-  Position,
-  type Node,
-  type NodeProps,
-} from "@xyflow/react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
-import { Button } from "./ui/button";
-import { CirclePlus, CircleX, GitBranch, Pencil } from "lucide-react";
+import { type Node, type NodeProps } from "@xyflow/react";
+import NodeMenu from "./node-menu";
 
 export type CustomNodeType = Node<{ name: string; comment?: string }>;
 
@@ -14,73 +7,7 @@ export default function Node(props: NodeProps<CustomNodeType>) {
   const isLeaf = props.type === "leaf";
   return (
     <>
-      <NodeToolbar isVisible={undefined} position={Position.Right}>
-        <nav className="grid gap-1 overflow-hidden rounded-xl border-2 border-border/50 bg-background">
-          {isLeaf ? (
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Add Node"
-                >
-                  <CirclePlus className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Add Node
-              </TooltipContent>
-            </Tooltip>
-          ) : (
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Branch Off"
-                >
-                  <GitBranch className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Branch Off
-              </TooltipContent>
-            </Tooltip>
-          )}
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="rounded-lg"
-                aria-label="Edit Node"
-              >
-                <Pencil className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Edit Node
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="rounded-lg"
-                aria-label="Delete Node"
-              >
-                <CircleX className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Delete Node
-            </TooltipContent>
-          </Tooltip>
-        </nav>
-      </NodeToolbar>
+      <NodeMenu isLeaf={isLeaf} />
       <div className="grid w-[8rem] min-w-[8rem] items-start gap-0 overflow-hidden rounded-lg border border-border/50 bg-background pt-1 text-xs shadow-xl transition-all ease-in-out hover:border-primary">
         <p className="truncate px-1.5 font-mono font-medium">
           {props.data.name}
