84e44e819496303286336abfee7592bec3428411
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-24 03:23:38 -0400

    Update comment model

--- src/models/comment-model.ts
+++ src/models/comment-model.ts
@@ -1,28 +1,35 @@
 import mongoose from "mongoose";
 
-export interface Comment {
+export interface IComment extends mongoose.Document {
   tag: string;
   text: string;
 }
 
-export interface MongoComment extends Comment, mongoose.Document {}
-
-export type TComment = Comment & {
-  _id: string;
-  createdAt: string;
-  updatedAt: string;
-};
-
-const CommentSchema = new mongoose.Schema<Comment>({
-  tag: {
-    type: String,
-    required: true,
+const CommentSchema = new mongoose.Schema<IComment>(
+  {
+    tag: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
   },
-  text: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
   },
-});
+);
 
-export default mongoose.models.Comment ||
-  mongoose.model<Comment>("Comment", CommentSchema);
+const Comment: mongoose.Model<IComment> =
+  mongoose.models?.Comment || mongoose.model("Comment", CommentSchema);
+
+export default Comment;
