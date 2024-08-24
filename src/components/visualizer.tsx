06d61cd082bf9432f465f22bef930ab636ea4d79
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-24 03:25:00 -0400

    Fetch data from database

--- src/components/visualizer.tsx
+++ src/components/visualizer.tsx
@@ -1,15 +1,18 @@
+import { api } from "@/trpc/server";
 import Three from "./three";
 import { ReactFlowProvider } from "@xyflow/react";
-import { api } from "@/trpc/server";
 
 export default async function Visualizer() {
   const nodes = await api.node.getNodes();
+  const nodesData = JSON.parse(JSON.stringify(nodes));
+  const comments = await api.comment.getComments();
+  const commentsData = JSON.parse(JSON.stringify(comments));
 
   return (
     <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
       <div className="relative flex h-full min-h-[50vh] w-full flex-col rounded-xl bg-[radial-gradient(#1e293b_1px,transparent_1px)] p-4 [background-size:20px_20px] lg:col-span-3">
         <ReactFlowProvider>
-          <Three data={{ nodes, comments: [] }} />
+          <Three data={{ nodes: nodesData, comments: commentsData }} />
         </ReactFlowProvider>
       </div>
     </main>
