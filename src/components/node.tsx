fd356fa9c9a5ce28ab952aff787fd5666d404e81
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-24 03:26:30 -0400

    Include d3Node in custom node type

--- src/components/node.tsx
+++ src/components/node.tsx
@@ -1,15 +1,19 @@
 import { type Node, type NodeProps } from "@xyflow/react";
 import NodeMenu from "./node-menu";
+import * as d3 from "d3";
 
-export type CustomNodeType = Node<{ name: string; comment?: string }>;
+export type CustomNodeType = Node<{
+  name: string;
+  comment?: string;
+  d3Node?: d3.HierarchyPointNode<CustomNodeType>;
+}>;
 
 export default function Node(props: NodeProps<CustomNodeType>) {
-  const isLeaf = props.type === "leaf";
   return (
     <>
-      <NodeMenu isLeaf={isLeaf} />
+      <NodeMenu {...props} />
       <div className="grid w-[8rem] min-w-[8rem] items-start gap-0 overflow-hidden rounded-lg border border-border bg-background pt-1 text-xs shadow-xl transition-all ease-in-out hover:border-primary">
-        <p className="truncate px-1.5 font-mono font-medium">
+        <p className="break-all px-1.5 font-mono font-medium">
           {props.data.name}
         </p>
         <div className="border-t border-border px-1.5 py-1">
