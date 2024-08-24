a89062ed90b11de8880274fd91b5e293c1cccee7
Ehsan Jso <ehsan.jsoure@gmail.com>
2024-08-24 03:23:03 -0400

    Reduce api wait time for dev

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -81,7 +81,7 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
 
   if (t._config.isDev) {
     // artificial delay in dev
-    const waitMs = Math.floor(Math.random() * 400) + 100;
+    const waitMs = Math.floor(Math.random() * 100) + 100;
     await new Promise((resolve) => setTimeout(resolve, waitMs));
   }
 
